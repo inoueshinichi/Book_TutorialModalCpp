1854f38f11a3876c5829da27f9728363b3bbd3de
Inoue Shinichi XPS13 9310 Ubuntu 20.04 LST <inoue.shinichi.1800@gmail.com>
2020-11-20 23:24:49 +0900

    First Commit

---
+++ all.hpp
@@ -0,0 +1,20 @@
+#ifndef __ALL_HPP__
+#define __ALL_HPP__
+
+#include <cstddef>
+#include <limits>
+#include <cfloat>
+#include <cstdint>
+#include <cstdlib>
+#include <new>
+#include <typeinfo>
+#include <exception>
+#include <initializer_list>
+#include <cstdalign>
+#include <stdexcept>
+#include <cassert>
+#include <cerrno>
+#include <system_error>
+#include <string>
+
+#endif
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/inoue/Desktop/Cpp_Tutorial
+# Build directory: /home/inoue/Desktop/Cpp_Tutorial/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
